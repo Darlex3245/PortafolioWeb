69dce7c1a8f18404f8f1b18cb67b5c2d415e45cf
Darlex3245 <darlex3245@gmail.com>
2024-11-02 18:03:08 -0600

    Cambio de rutas

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './src/css/style.css';
-import { Header } from './src/components/Header';
-import { Main } from './src/components/Main';
-import { Footer } from './src/components/Footer';
+import '/src/css/style.css';
+import { Header } from '/src/components/Header';
+import { Main } from '/src/components/Main';
+import { Footer } from '/src/components/Footer';
 
 // Inserta los componentes en las áreas correspondientes
 document.querySelector('header').innerHTML = Header();
